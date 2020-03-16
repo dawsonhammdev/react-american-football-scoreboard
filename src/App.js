26bb4b04d32ef6503891de73e67cfed25d86448b
dawsonhammdev <hammsdesigns@gmail.com>
2020-03-16 14:05:43 -0500

    I have added my useStat hooks for both home and away scores

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import React, {useState} from "react";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+    const [ countHomeScore, setCountHomeScore ] = useState(0);
+    const [ countAwayScore, setCountAwayScore ] = useState(0);
 
   return (
     <div className="container">
